027138269a38e1131da8d6e331047fafa8bf999d
karvel-code <velgithinji@gmail.com>
2021-11-03 09:42:12 +0300

    Add columns for items in the migration file

--- db/migrate/20211103063620_create_products.rb
+++ db/migrate/20211103063620_create_products.rb
@@ -1,6 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.string :price
+      t.string :picture
 
       t.timestamps
     end
