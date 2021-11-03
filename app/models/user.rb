98e54a9c5cb296de0f665bfbe1506d01d598f3b2
karvel-code <velgithinji@gmail.com>
2021-11-03 23:56:37 +0300

    Add columns for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+
+    enum role: [:admin, :normalUser]
 end
--- db/migrate/20211103205158_create_users.rb
+++ db/migrate/20211103205158_create_users.rb
@@ -1,6 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      t.string :firstName
+      t.string :middleName
+      t.string :lastName
+      t.string :email
 
       t.timestamps
     end
