48045c1c3d5f17e18e6fb617c9fe20a43d5d80cb
karvel-code <velgithinji@gmail.com>
2021-11-04 15:42:39 +0300

    Add create method for product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::ProductsController < ApplicationController
+
+    def create
+        @product = Product.new(product_params)
+
+        if @product.save
+            render json: @product, status: :created
+        else
+            render json: @product.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def product_params
+        params.require(:product).permit(:name, :description, :price, :picture, :quantity)
+    end
 end
