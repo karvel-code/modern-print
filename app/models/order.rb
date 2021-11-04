ecb7af1d9f8b7cfb900ba7f7195267048d26e96e
karvel-code <velgithinji@gmail.com>
2021-11-04 15:06:23 +0300

    Add set order number method in order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
 
     enum deliveryOption: [:delivery, :pickUp]
+
+    def set_order_number!
+        loop do
+          self.orderNumber ||= "#{Time.now}#{SecureRandom.hex(6)}"
+          break unless self.class.exists?(orderNumber: orderNumber)
+        end
+        self.save
+    end
 end
